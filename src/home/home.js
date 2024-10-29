eefa1f9a06d4fb11277a7937da1b48d092b6bfc1
Boshra Nabaei <boshranabaei@Boshras-MacBook-Air.local>
2024-10-28 23:20:39 -0700

    added images for the home page

--- src/home/home.js
+++ src/home/home.js
@@ -1,7 +1,12 @@
 import './home.scss';
 import {
-    HOME_P1_1, HOME_P1_2, CHECKLIST_DESIGN, CHECKLIST_PROGRAMMING
+    HOME_P1_1, HOME_P1_2, CHECKLIST_DESIGN, CHECKLIST_PROGRAMMING,
+    HOME_S1, HOME_S2, HOME_S3, HOME_S4
 } from '../assets/strings/constantTitles';
+import homeSum1 from "../assets/img/home-sum-1.jpg";
+import homeSum2 from "../assets/img/home-sum-2.png";
+import homeSum3 from "../assets/img/home-sum-3.png";
+import homeSum4 from "../assets/img/home-sum-4.png";
 
 function Home() {
     return (
@@ -11,9 +16,29 @@ function Home() {
           <p className="supreme fade-in">{HOME_P1_2}</p>
         </div>
 
+        <div class="home-summary">
+          <img src={homeSum1} alt="sss"/>
+          <div>{HOME_S1}</div>
+        </div>
+
+        <div class="home-summary">
+          <img src={homeSum2} alt="sss"/>
+          <div>{HOME_S2}</div>
+        </div>
+
+        <div class="home-summary">
+          <img src={homeSum3} alt="sss"/>
+          <div>{HOME_S3}</div>
+        </div>
+
+        <div class="home-summary">
+          <img src={homeSum4} alt="sss"/>
+          <div>{HOME_S4}</div>
+        </div>
+
         <div class="flex-container">
-          <div className="checklist"><i className="fa-solid fa-list-check"></i>{CHECKLIST_DESIGN}</div>
-          <div className="checklist"><i className="fa-solid fa-list-check"></i>{CHECKLIST_PROGRAMMING}</div>
+          <a href="/" className="checklist"><i className="fa-solid fa-list-check"></i>{CHECKLIST_DESIGN}</a>
+          <a href="/" className="checklist"><i className="fa-solid fa-list-check"></i>{CHECKLIST_PROGRAMMING}</a>
         </div>
           {/* <video controls autoPlay muted loop>
               <source src={tabVideo} type="video/mp4"/>
