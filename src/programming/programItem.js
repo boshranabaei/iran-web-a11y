42d6d652ae6235589d6142738346d26e7ffa5e25
System Administrator <root@Boshras-MacBook-Air.local>
2024-12-13 10:43:10 -0800

    added new programming content

--- src/programming/programItem.js
+++ src/programming/programItem.js
@@ -13,32 +13,38 @@ function ProgramItem({item, programList}) {
         <div className='design-item'>
           <p>{item.content.DETAIL}</p>
 
-          <h3>{item.content.T1}</h3>
+          {item.content.T1 && <h3>{item.content.T1}</h3>}
           
-          <p>{item.content.P1}</p>
+          {item.content.P1 && <p>{item.content.P1}</p>}
 
-          <SyntaxHighlighter language="javascript" style={dark}>
-            {item.content.CODE1}
-          </SyntaxHighlighter>
+          {item.content.CODE1 && 
+            <SyntaxHighlighter language="javascript" style={dark}>
+              {item.content.CODE1}
+            </SyntaxHighlighter>}
 
-          <p>{item.content.P2}</p>
+          {item.content.P2 && <p>{item.content.P2}</p>}
 
-          <SyntaxHighlighter language="javascript" style={dark}>
-            {item.content.CODE2}
-          </SyntaxHighlighter>
+          {item.content.CODE2 &&
+            <SyntaxHighlighter language="javascript" style={dark}>
+              {item.content.CODE2}
+            </SyntaxHighlighter>
+          }
 
-          <p>{item.content.IMG_REF}</p>
-
-          <figure>
-            <img className="single" src={`${item.contentImage.IMG}`} alt={item.contentImage.IMG_ALT}></img>
-            <figcaption>{item.contentImage.IMG_CPT}</figcaption>
-          </figure>
+          {item.content.IMG_REF && 
+            <>
+            <p>{item.content.IMG_REF}</p>
+            <figure>
+              <img className="single" src={`${item.contentImage.IMG}`} alt={item.contentImage.IMG_ALT}></img>
+              <figcaption>{item.contentImage.IMG_CPT}</figcaption>
+            </figure>
+            </>
+          }
           <div className='link'><i className="fa-solid fa-link"></i>{programList.USEFUL_LINKS}
             {item.links?.map((link, index) => (
               <a key={index} href={link.URL}>{link.TITLE}</a>
               ))}
           </div>
-          {item.right.CONTENT &&
+          {item.right &&
             <>
             <div className='flexContent'>
               <div className="wrongExp">
